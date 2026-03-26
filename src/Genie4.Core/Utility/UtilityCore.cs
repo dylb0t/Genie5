e2010c65a7f558e2c05b35ac3627d6838b455e49
dylb0t <36675473+dylb0t@users.noreply.github.com>
2026-03-26 12:17:40 -0700

    Add UtilityCore helpers

---
+++ src/Genie4.Core/Utility/UtilityCore.cs
@@ -0,0 +1,39 @@
+using System.Globalization;
+
+namespace Genie4.Core.Utility;
+
+public static class UtilityCore
+{
+    public static bool StringToBoolean(string? value)
+    {
+        return value switch
+        {
+            "True" or "true" or "1" => true,
+            _ => false
+        };
+    }
+
+    public static double StringToDouble(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return -1;
+        }
+
+        return double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out var parsed)
+            ? parsed
+            : -1;
+    }
+
+    public static int StringToInteger(string? value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return -1;
+        }
+
+        return int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var parsed) && parsed > 0
+            ? parsed
+            : -1;
+    }
+}
